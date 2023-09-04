f80ec3972c9cdd501163f8c1f69fe524cb7b41b5
Matthew Wilson <matthew.wilson@methods.co.uk>
2023-09-04 17:23:15 +0100

    Update links in help section and main nav
    Update navigation links in the help section and navigation links on the main dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,12 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
