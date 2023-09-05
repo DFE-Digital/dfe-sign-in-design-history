76b0345f1c62a23623a23cfa54c791b492e06fb7
Matthew Wilson <matthew.wilson@methods.co.uk>
2023-09-05 14:57:34 +0100

    Add multi-factor authentication feature
    Add the multi-factor authentication feature to the Manage Console prototype

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,51 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/mfa-configuration-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var mfaConfiguration = req.session.data['mfa-configuration']
+  
+    // Check whether the variable matches a condition
+    if (mfaConfiguration == "Turn on"){
+      // Send user to next page
+      res.redirect('/manage-console/mfa/turn-on-mfa')
+    } else if (mfaConfiguration == "Turn off"){
+      // Send user to ineligible page
+      res.redirect('/manage-console/mfa/turn-off-mfa')
+    }
+  })
+
+  // Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/turn-on-mfa-answer-manage', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var turnOffMfa = req.session.data['turn-off-mfa']
+  
+    // Check whether the variable matches a condition
+    if (turnOffMfa == "Yes"){
+      // Send user to next page
+      res.redirect('/manage-console/mfa/dashboard-turn-on-banner')
+    } else if (turnOffMfa == "No"){
+      // Send user to ineligible page
+      res.redirect('/manage-console/manage-dashboard')
+    }
+  })
+
+  // Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/turn-off-mfa-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var turnOffMfa = req.session.data['turn-off-mfa']
+  
+    // Check whether the variable matches a condition
+    if (turnOffMfa == "Yes"){
+      // Send user to next page
+      res.redirect('/manage-console/mfa/dashboard-turn-off-banner')
+    } else if (turnOffMfa == "No"){
+      // Send user to ineligible page
+      res.redirect('/manage-console/manage-dashboard')
+    }
+  })
