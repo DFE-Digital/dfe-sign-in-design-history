33e2fcbe13486366166bcf53ae93fdbb803dc85e
Matthew Wilson <matthew.wilson@methods.co.uk>
2023-09-18 12:46:56 +0100

    Create the create policy journey
    Create the create policy journey for both user type policies and org type policies

--- app/routes.js
+++ app/routes.js
@@ -81,10 +81,10 @@ router.post('/service-type-answer', function (req, res) {
     // Check whether the variable matches a condition
     if (policyType == "Organisation"){
       // Send user to next page
-      res.redirect('/manage-console/v1/manage-policies/organisation-conditions/add-condition')
+      res.redirect('/manage-console/v1/manage-policies/create-policy/organisation-conditions/add-condition')
     } else if (policyType == "User"){
       // Send user to ineligible page
-      res.redirect('/manage-console/v1/manage-policies/user-conditions/add-condition')
+      res.redirect('/manage-console/v1/manage-policies/create-policy/user-conditions/add-condition')
     }
   })
 
@@ -97,18 +97,53 @@ router.post('/service-type-answer', function (req, res) {
       // Check whether the variable matches a condition
       if (conditionType == "Establishment"){
         // Send user to next page
-        res.redirect('/manage-console/v1/manage-policies/organisation-conditions/establishment-type')
+        res.redirect('/manage-console/v1/manage-policies/create-policy/organisation-conditions/establishment-type')
       } else if (conditionType == "Category"){
         // Send user to ineligible page
-        res.redirect('#')
+        res.redirect('/manage-console/v1/manage-policies/create-policy/organisation-conditions/organisation-category')
       } else if (conditionType == "Organisation"){
         // Send user to ineligible page
-        res.redirect('#')
+        res.redirect('/manage-console/v1/manage-policies/create-policy/organisation-conditions/organisation-id')
       }else if (conditionType == "Status"){
         // Send user to ineligible page
-        res.redirect('#')
+        res.redirect('/manage-console/v1/manage-policies/create-policy/organisation-conditions/organisation-status')
       }else if (conditionType == "No condition"){
         // Send user to ineligible page
-        res.redirect('/manage-console/v1/manage-policies/organisation-conditions/policy-name')
+        res.redirect('/manage-console/v1/manage-policies/create-policy/organisation-conditions/policy-name')
       }
     })
+
+      // Run this code when a form is submitted to 'juggling-balls-answer'
+  router.post('/user-condition-type-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'how-many-balls'
+    var conditionType = req.session.data['condition-type']
+  
+    // Check whether the variable matches a condition
+    if (conditionType == "Permission"){
+      // Send user to next page
+      res.redirect('/manage-console/v1/manage-policies/create-policy/user-conditions/permission-level')
+    } else if (conditionType == "Email"){
+      // Send user to ineligible page
+      res.redirect('/manage-console/v1/manage-policies/create-policy/user-conditions/email-address')
+    } else if (conditionType == "No condition"){
+      // Send user to ineligible page
+      res.redirect('/manage-console/v1/manage-policies/create-policy/user-conditions/policy-name')
+    }
+  })
+
+      // Run this code when a form is submitted to 'juggling-balls-answer'
+      router.post('/permission-level-answer', function (req, res) {
+
+        // Make a variable and give it the value from 'how-many-balls'
+        var permissionLevel = req.session.data['permission-level']
+      
+        // Check whether the variable matches a condition
+        if (permissionLevel == "End user"){
+          // Send user to next page
+          res.redirect('/manage-console/v1/manage-policies/create-policy/user-conditions/add-another-condition')
+        } else if (permissionLevel == "Approver"){
+          // Send user to ineligible page
+          res.redirect('/manage-console/v1/manage-policies/create-policy/user-conditions/add-another-condition')
+        }
+      })
