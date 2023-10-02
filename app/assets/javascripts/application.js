e4a31df4e25f0ef88a6bd4b932d5579e8ca294d5
Matthew Wilson <matthew.wilson@methods.co.uk>
2023-10-02 15:08:05 +0100

    Create DfE Sign-in prototype version
    Create a version of DfE Sign-in prototype - Version 1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -131,3 +131,70 @@ function removeRow6() {
     x.style.display = "none";
   }
 }
+
+function ShowAndChange() {
+  var x = document.getElementById('SectionName');
+  if (x.style.display == 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+}
+
+
+var myInput = document.getElementById("password");
+var letter = document.getElementById("length-newPassword");
+var capital = document.getElementById("upper-case-newPassword");
+var number = document.getElementById("numbers-newPassword");
+var lowercase = document.getElementById("lower-case-newPassword");
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate password length
+  if(myInput.value.length >= 8) {
+    letter.classList.remove("icon-remove");
+    letter.classList.add("icon-ok");
+  } else {
+    letter.classList.remove("icon-ok");
+    letter.classList.add("icon-remove");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z].*[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("icon-remove");
+    capital.classList.add("icon-ok");
+  } else {
+    capital.classList.remove("icon-ok");
+    capital.classList.add("icon-remove");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9].*[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("icon-remove");
+    number.classList.add("icon-ok");
+  } else {
+    number.classList.remove("icon-ok");
+    number.classList.add("icon-remove");
+  }
+
+  // Validate lowercase
+  var lowerCaseLetters = /[a-z].*[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    lowercase.classList.remove("icon-remove");
+    lowercase.classList.add("icon-ok");
+  } else {
+    lowercase.classList.remove("icon-ok");
+    lowercase.classList.add("icon-remove");
+  }
+}
+
+function confirmPassword() {
+  var y = document.getElementById("confirm-password");
+    if (y.type === "password") {
+      y.type = "text";
+    } else {
+      y.type = "password";
+    }
+  }
